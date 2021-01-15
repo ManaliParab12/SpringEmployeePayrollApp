b4eaef944235c6545fcb9a76f6c5b9a0917a0c77
ManaliParab12 <manaliparab10@gmail.com>
2021-01-15 13:18:02 +0530

    Created Employee Payroll Spring Project to cater to REST Request

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Payroll Application");
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
 	}
 
