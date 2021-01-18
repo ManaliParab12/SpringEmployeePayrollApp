3a61fc9c74fbc82a98a73208bd38b3b1a6a1e5ad
ManaliParab12 <manaliparab10@gmail.com>
2021-01-18 23:07:39 +0530

    Added not empty field and pattern validation to Name Field

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,11 +1,18 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 public class EmployeePayrollDTO {
+
+	@NotEmpty(message = "Employee name cannot be null")
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
 	public String name;
+
+	@Min(value = 500, message = "Min Wage should be more than 500")
 	public long salary;
 	
-
 	public EmployeePayrollDTO(String name, long salary) {
 		super();
 		this.name = name;
@@ -16,5 +23,5 @@ public class EmployeePayrollDTO {
 	@Override
 	public String toString() {
 		return "name=" +name+":salary="+salary;
-}
+	}
 }
