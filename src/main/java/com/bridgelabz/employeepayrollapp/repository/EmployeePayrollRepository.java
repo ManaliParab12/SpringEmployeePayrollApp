83c6ca6761822e35de9dcf832fc06cb136494033
ManaliParab12 <manaliparab10@gmail.com>
2021-01-20 21:44:18 +0530

    Added new Method in repository interface with @Query Annotation for Native Queries

--- src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
@@ -5,5 +5,6 @@ import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer> {
-    
+    @Query(value="select * from employee_payroll, employee_department where employee_id = id and department = :department", nativeQuery = true)
+    List<EmployeePayrollData> findEmployeesByDepartment(String department);    
 }
