f2bbb186533dc198bee8649491096d4068699506
ManaliParab12 <manaliparab10@gmail.com>
2021-01-20 11:16:14 +0530

    Added gender, startDate, note, profilePic and department in DTO and Model

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,41 +1,31 @@
 package com.bridgelabz.employeepayrollapp.model;
 
-import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import java.time.LocalDate;
+import java.util.List;
 
-public class EmployeePayrollData {
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import lombok.Data;
+
+public @Data class EmployeePayrollData {
     private int employeeId;
 	private String name;
 	private long salary;
+	private String gender;
+	private LocalDate startDate;
+	private String note;
+	private String profilePic;
+	private List<String> departments;
 	
-	public EmployeePayrollData() { }
-	
+	public EmployeePayrollData() {}
+
 	public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
 		this.employeeId = empId;
 		this.name = empPayrollDTO.name;
 		this.salary = empPayrollDTO.salary;
+		this.gender = empPayrollDTO.gender;
+		this.startDate = empPayrollDTO.startDate;
+		this.note = empPayrollDTO.note;
+		this.profilePic = empPayrollDTO.profilePic;
+		this.departments = empPayrollDTO.department;
 	}
-	
-	public int getEmployeeId() {
-		return employeeId;
-	}
-
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public long getSalary() {
-		return salary;
-	}
-
-	public void setSalary(long salary) {
-		this.salary = salary;
-	} 
 }
